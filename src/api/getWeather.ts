1f5e437ab0f42f16527b9ae8ea63b427149b1b3f
Davidson <vader@assoweb.com.br>
2023-11-16 15:47:26 -0300

    fix: weather api https url

--- src/api/getWeather.ts
+++ src/api/getWeather.ts
@@ -1,6 +1,6 @@
 export const getWeather = async (search: string) => {
   const result = fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${
+    `https://api.weatherapi.com/v1/current.json?key=${
       import.meta.env.VITE_WEATHER_APIKEY
     }&q=${search}&aqi=yes`
   )
